b825a15b7b3f84b9522f69c59a4893d63d4ec3b9
srathod <saurabh_rathod@hotmail.com>
2014-05-29 11:49:14 -0400

    ServerConnection
    Method to connect to the server

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,7 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
 
 public class Test {
 
@@ -6,6 +10,32 @@ public class Test {
 		System.out.println("Nod if you see this!!!!!");
 		System.out.println("No");
 
+	}
+	
+	private String username = "tester";
+	
+	public Connection getConnection() throws SQLException {
+
+	    Connection conn = null;
+	    Properties connectionProps = new Properties();
+	    connectionProps.put("user", this.userName);
+	    connectionProps.put("password", this.password);
+
+	    if (this.dbms.equals("mysql")) {
+	        conn = DriverManager.getConnection(
+	                   "jdbc:" + this.dbms + "://" +
+	                   this.serverName +
+	                   ":" + this.portNumber + "/",
+	                   connectionProps);
+	    } else if (this.dbms.equals("derby")) {
+	        conn = DriverManager.getConnection(
+	                   "jdbc:" + this.dbms + ":" +
+	                   this.dbName +
+	                   ";create=true",
+	                   connectionProps);
+	    }
+	    System.out.println("Connected to database");
+	    return conn;
 	}
 
 }
