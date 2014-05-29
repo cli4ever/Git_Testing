d9a36884377746bd04d189a167ab9e77edd83b34
srathod <saurabh_rathod@hotmail.com>
2014-05-29 12:05:32 -0400

    ChangedConnector
    -

--- src/Test.java
+++ src/Test.java
@@ -1,41 +1,11 @@
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Properties;
-
 
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("YOO WAZZZZUPPPPP!!!!");
-		System.out.println("Nod if you see this!!!!!");
-		System.out.println("No");
+//can you see this ?// this will give error
+		//Saw it, changed it!
+		//Fixed Error!
 
 	}
-	
-	private String username = "tester";
-	
-	public Connection getConnection() throws SQLException {
-
-	    Connection conn = null;
-	    Properties connectionProps = new Properties();
-	    connectionProps.put("user", this.userName);
-	    connectionProps.put("password", this.password);
-
-	    if (this.dbms.equals("mysql")) {
-	        conn = DriverManager.getConnection(
-	                   "jdbc:" + this.dbms + "://" +
-	                   this.serverName +
-	                   ":" + this.portNumber + "/",
-	                   connectionProps);
-	    } else if (this.dbms.equals("derby")) {
-	        conn = DriverManager.getConnection(
-	                   "jdbc:" + this.dbms + ":" +
-	                   this.dbName +
-	                   ";create=true",
-	                   connectionProps);
-	    }
-	    System.out.println("Connected to database");
-	    return conn;
-	}
-
 }
